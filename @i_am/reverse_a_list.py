bab6c0c0a1aa32a9dccefbeffb7ff0ab55148c90
semich11 <chima11christopher@gmail.com>
2024-07-30 11:59:38 +0100

    push

--- @i_am/reverse_a_list.py
+++ @i_am/reverse_a_list.py
@@ -8,10 +8,19 @@ newList = [1,2,3,4]
 def reverseAListMethod(newList):
 
 		reverseList = []
-		for count in range(4):
+		for count in range(len(newList)):
 			reverseList += [newList[len(newList) - count - 1]]
 		return reverseList;	
 
 
 print(reverseAListMethod(newList))
 
+
+
+newList = [1,2,3,4]
+
+
+otherList = [5,6,7,8]
+for count in range(len(newList)):
+	otherList += [newList[count]
+print(otherList)
---
+++ christopher_chima/part2/task1.py
@@ -0,0 +1,35 @@
+
+Step 01: create a variable that will hold string.
+Step 02: use for loop to loop through all the range of numbers
+Step 03: for each number gotten from the loop, convert the number to string
+Step 04: use a for loop to loop the amount of the length of the string in Step 02
+Step 05: collect element from the string variable and convert it to integer
+Step 06: add the integer gotten from Step 05: to the variable declare in Step 01
+Step 07: convert the variable in Step 01 to integer
+Step 08: if the integer gotten in step 05 is greater than 0 break the loop
+
+
+
+
+
+
+tempHolder = ""
+
+
+
+for number in range(1000, 3001):
+	str_number = str(number)
+	for _ in range(len(str_number)):
+		parse_to_int = int(str_number[_])
+		tempHolder += str(parse_to_int)
+		all_to_int = int(tempHolder)
+		if parse_to_int % 2 > 0:
+			break;
+	if all_to_int % 2 == 0:	
+		print(number, end=", ")
+
+
+
+
+
+	
